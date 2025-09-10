771c5a3c1c532f218a0e136b9ee5bd1965c94fc4
robbytrujillo <robbyikusuma88@gmail.com>
2025-09-10 11:31:19 +0700

    Introduction BE api-tick-stack

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,7 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Container\Attributes\Auth;
+use Exception;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use PhpParser\Node\Stmt\TryCatch;
 
@@ -10,17 +11,28 @@ class AuthController extends Controller
 {
     public function login(Request $request) {
         try {
-            if (!Auth::guard('web')->attemp($request->only('email','password'))) {
+            if (!Auth::guard('web')->attempt($request->only('email','password'))) {
                 return response()->json([
-                    'message' => 'Unautorized',
+                    'message' => 'Unauthorized',
                     'data' => null
                 ], 401);
             }
 
             $user = Auth::user();
             $token = $user->createToken('auth_token')->plainTextToken;
-        } catch (\Throwable $th) {
-            
+
+             return response()->json([
+                    'message' => 'Login Berhasil!',
+                    'data' => [
+                        'token' => $token,
+                        'user' => $user
+                    ]
+                ], 200);
+        } catch (Exception $e) {
+             return response()->json([
+                    'message' => 'Terjadi Kesalahan',
+                    'error' => $e->getMessage()
+                ], 500);
         }
     }
 }
