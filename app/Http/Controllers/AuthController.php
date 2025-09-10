08969d23b35b616fda5d1da217cf64d1365c8234
robbytrujillo <robbyikusuma88@gmail.com>
2025-09-10 08:34:52 +0700

    Introduction BE api-tick-stack

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -20,7 +20,7 @@ class AuthController extends Controller
             $user = Auth::user();
             $token = $user->createToken('auth_token')->plainTextToken;
         } catch (\Throwable $th) {
-            //throw $th;
+            
         }
     }
 }
